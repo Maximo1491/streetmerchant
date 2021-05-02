33d83b117585e95f3249063ad2a5efbe92019a26
Max Morris <maximo1491@hotmail.com>
2021-05-02 20:00:34 +0100

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -64,7 +64,7 @@ export async function usingPage<T>(
 ): Promise<T> {
   const page = await browser.newPage();
   page.setDefaultNavigationTimeout(config.page.timeout);
-  await page.setUserAgent(await getRandomUserAgent());
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0');
 
   try {
     return await cb(page, browser);
