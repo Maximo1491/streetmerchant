0db56384a739e4bca21a03a1b80bda097d469cd5
Max Morris <maximo1491@hotmail.com>
2021-08-03 21:25:28 +0100

    removing stuff thats now constantly in stock

--- src/store/model/amazon-uk.ts
+++ src/store/model/amazon-uk.ts
@@ -29,17 +29,6 @@ export const AmazonUk: Store = {
   links: [
     {
       brand: 'pokemon',
-      model: 'booster-box',
-      description: 'Battle Styles Booster Box',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/81vbpp-b2MS._AC_SL1500_.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08R84814V&Quantity.1=1',
-      series: 'battle-styles',
-      url: 'https://www.amazon.co.uk/gp/product/B08R84814V/',
-      maxPrice: 100
-    },
-    {
-      brand: 'pokemon',
       model: 'tin',
       description: 'Hidden Fates Random Tin',
       thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/71mvWo6R8yL._AC_SL1248_.jpg',
@@ -96,94 +85,6 @@ export const AmazonUk: Store = {
     {
       brand: 'pokemon',
       model: 'etb',
-      description: 'Shining Fates Elite Trainer Box',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/712jg2dyxEL._AC_SL1500_.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08PS2F9RD&Quantity.1=1',
-      series: 'shining-fates',
-      url: 'https://www.amazon.co.uk/gp/product/B08PS2F9RD/',
-      maxPrice: 53
-    },
-    {
-      brand: 'pokemon',
-      model: 'mini-tin',
-      description: 'Shining Fates Mini Tin',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/91qV1ehvzeL._AC_SL1500_.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08PTTVLBJ&Quantity.1=1',
-      series: 'shining-fates',
-      url: 'https://www.amazon.co.uk/gp/product/B08PTTVLBJ/',
-      maxPrice: 10
-    },
-    {
-      brand: 'pokemon',
-      model: 'box',
-      description: 'Shining Fates Random Mad Party Pin Collection',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/71LmmJrFwkL._AC_SL1000_.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08PTV374B&Quantity.1=1',
-      series: 'shining-fates',
-      url: 'https://www.amazon.co.uk/gp/product/B08PTV374B/',
-      maxPrice: 16
-    },
-    {
-      brand: 'pokemon',
-      model: 'box',
-      description: 'Shining Fates Pikachu V Box',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/617x1O-W2lL._AC_.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08PS15TSH&Quantity.1=1',
-      series: 'shining-fates',
-      url: 'https://www.amazon.co.uk/gp/product/B08PS15TSH/',
-      maxPrice: 25
-    },
-    {
-      brand: 'pokemon',
-      model: 'box',
-      description: 'Shining Fates Crobat/Dragapult Premium Collection',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/71oG815Mm5L._AC_SL1500_.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08PSBQPTV&Quantity.1=1',
-      series: 'shining-fates',
-      url: 'https://www.amazon.co.uk/gp/product/B08PSBQPTV/',
-      maxPrice: 42
-    },
-    {
-      brand: 'pokemon',
-      model: 'tin',
-      description: 'Shining Fates Random Tin',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/51M1bHJ9srL._AC_.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08PS582QY&Quantity.1=1',
-      series: 'shining-fates',
-      url: 'https://www.amazon.co.uk/gp/product/B08PS582QY/',
-      maxPrice: 30
-    },
-    {
-      brand: 'pokemon',
-      model: 'tin',
-      description: 'Shining Fates Random Tin',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/51M1bHJ9srL._AC_.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08PS582QY&Quantity.1=1',
-      series: 'shining-fates',
-      url: 'https://www.amazon.co.uk/gp/product/B08RDV1GVB/',
-      maxPrice: 30
-    },
-    {
-      brand: 'pokemon',
-      model: 'etb',
-      thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/51allUwURLL._AC_SL1024_.jpg',
-      description: 'Zacian/Zamazenta Elite Trainer Box',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08HJCJT89&Quantity.1=1',
-      series: 'sword-and-shield',
-      url: 'https://www.amazon.co.uk/gp/product/B08HJCJT89/',
-      maxPrice: 65
-    },
-    {
-      brand: 'pokemon',
-      model: 'etb',
       thumbnail: 'https://images-na.ssl-images-amazon.com/images/I/61B9mMB6oML._AC_SL1500_.jpg',
       description: 'Zacian and Zamazenta Ultra Premium Collection',
       cartUrl:
@@ -191,17 +92,6 @@ export const AmazonUk: Store = {
       series: 'sword-and-shield',
       url: 'https://www.amazon.co.uk/gp/product/B08HJ86Y8T/',
       maxPrice: 120
-    },
-    {
-      brand: 'pokemon',
-      model: 'etb',
-      description: 'Battle Styles Elite Trainer Box',
-      thumbnail: 'https://cdn.shopify.com/s/files/1/1381/2481/products/unnamed_11_487b850d-7eb6-458d-a277-c0f6a35286a4.jpg',
-      cartUrl:
-        'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08QZM514Y&Quantity.1=1',
-      series: 'battle-styles',
-      url: 'https://www.amazon.co.uk/gp/product/B08QZM514Y/',
-      maxPrice: 43
     },
     {
       brand: 'pokemon',
@@ -245,7 +135,7 @@ export const AmazonUk: Store = {
         'https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=B08GZ7MWFV&Quantity.1=1',
       series: 'vivid-voltage',
       url: 'https://www.amazon.co.uk/gp/product/B08GZ7MWFV/',
-      maxPrice: 140
+      maxPrice: 110
     },
     {
       brand: 'pokemon',
